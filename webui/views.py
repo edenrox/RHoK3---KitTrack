9ca62f9f7f90d05715873de06997c5e4e8805a4c
Ian Hopkins <i.hopkins@syncapse.com>
2011-06-05 13:57:45 -0400

    working on the sms interpreter

--- webui/views.py
+++ webui/views.py
@@ -95,7 +95,7 @@ def sms(request):
 			# ok, what packages are in trasit
 			location = Location.objects.get(pk=tijuana_id)
 			kits = Kit.objects.get(destination=location.pk)
-			response_text = u'Kits on the way to %s:\n%s' % (location, ','.join(kits))
+			response_text = u'Kits on the way to %s:\n' % (location) #, ','.join(kits))
 		if (parts.length):
 			# load the kit
 			kit_id = parts[1]
