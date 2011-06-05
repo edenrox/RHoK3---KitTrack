eec5a6b565891c5430a7281bf59b67a1d852a028
Ian Hopkins <i.hopkins@syncapse.com>
2011-06-05 13:29:38 -0400

    working on the sms part

--- webui/views.py
+++ webui/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import *
 from django.core.context_processors import csrf
+from django.views.decorators.csrf import csrf_exempt
 from webui.models import *
 
 def kit_ship(request):
