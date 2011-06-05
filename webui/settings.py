c7489012837a57a7d76cfd57324add43690a6e36
Ian Hopkins <i.hopkins@syncapse.com>
2011-06-04 21:41:22 -0400

    trying to see if i can get the admin going

--- webui/settings.py
+++ webui/settings.py
@@ -109,17 +109,17 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
-	'django.contrib.admin',
-    'django.contrib.auth',
+	'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'webui',
 )
 
 # A sample logging configuration. The only tangible logging
