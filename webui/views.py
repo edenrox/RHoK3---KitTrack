fb6c8aeaa0d33d65d0a7a2d8d064ea3001306684
Ian Hopkins <i.hopkins@syncapse.com>
2011-06-05 11:20:00 -0400

    working on templates

--- webui/views.py
+++ webui/views.py
@@ -2,8 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
 def kit_ship(request):
-	return render_to_response('templates/ship.html');
+	return render_to_response('kit-ship.html')
 	
 def kit_track(request):
-	return render_to_response('templates/track.html');
-		
+	return render_to_response('kit-track.html')
+
+def kit_history(request, kit_id):
+	return render_to_response('kit-history.html')
