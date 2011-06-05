ca9d1dbc5a32877f62e3dfb0fdfa0a5b9c4761ff
Ian Hopkins <i.hopkins@syncapse.com>
2011-06-04 21:45:23 -0400

    fixing models

--- webui/models.py
+++ webui/models.py
@@ -13,28 +13,32 @@ class Kit(models.Model):
 	destintation = models.ForeignKey(Location)
 	estimated_delivery_date = models.DateTimeField()
 
+class KitState(models.Model):
+	name = models.CharField(max_length=255)
+	ordinal = models.IntegerField()
+
 class KitHistory(models.Model):
 	kit = models.ForeignKey(Kit)
 	created = models.DateTimeField()
 	location = models.ForeignKey(Location)
 	state = models.ForeignKey(KitState)
 	
-class KitState(models.Model):
-	name = models.CharField(max_length=255)
-	ordinal = models.IntegerField()
-
 class CartonType(models.Model):
 	type = models.ForeignKey(KitType)
 	name = models.CharField(max_length=255)
+
+class SupplyItemType(models.Model):
+	name = models.CharField(max_length=255)
+	
+class SupplyItem(models.Model):
+	name = models.CharField(max_length=255)
+	type = models.ForeignKey(SupplyItemType)
 
 class CartonContent(models.Model):
 	type = models.ForeignKey(CartonType)
 	item = models.ForeignKey(SupplyItem)
 	quantity = models.FloatField()
+
+
 	
-class SupplyItem(models.Model):
-	name = models.CharField(max_length=255)
-	type = models.ForeignKey(SupplyItemType)
-	
-class SupplyItemType(models.Model):
-	name = models.CharField(max_length=255)
+
