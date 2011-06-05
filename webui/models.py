5b772b36657965184f27e715f83795a4a4f93355
Ian Hopkins <i.hopkins@syncapse.com>
2011-06-04 21:43:17 -0400

    trying to see if i can get the admin going

--- webui/models.py
+++ webui/models.py
@@ -1,40 +1,40 @@
 from django.db import models
 
-class KitType(models.model):
+class KitType(models.Model):
 	name = models.CharField(max_length=255)
 
-class Location(models.model):
+class Location(models.Model):
 	name = models.CharField(max_length=255)
 	lat = models.DoubleField()
 	lng = models.DoubleField()
 
-class Kit(models.model):
+class Kit(models.Model):
 	kit_type = models.ForeignKey(KitType)
 	destintation = models.ForeignKey(Location)
 	estimated_delivery_date = models.DateTimeField()
 
-class KitHistory(models.model):
+class KitHistory(models.Model):
 	kit = models.ForeignKey(Kit)
 	created = models.DateTimeField()
 	location = models.ForeignKey(Location)
 	state = models.ForeignKey(KitState)
 	
-class KitState(models.model):
+class KitState(models.Model):
 	name = models.CharField(max_length=255)
 	ordinal = models.IntegerField()
 
-class CartonType(models.model):
+class CartonType(models.Model):
 	type = models.ForeignKey(KitType)
 	name = models.CharField(max_length=255)
 
-class CartonContent(models.model):
+class CartonContent(models.Model):
 	type = models.ForeignKey(CartonType)
 	item = models.ForeignKey(SupplyItem)
 	quantity = models.DoubleField()
 	
-class SupplyItem(models.model):
+class SupplyItem(models.Model):
 	name = models.CharField(max_length=255)
 	type = models.ForeignKey(SupplyItemType)
 	
-class SupplyItemType(models.model):
+class SupplyItemType(models.Model):
 	name = models.CharField(max_length=255)
