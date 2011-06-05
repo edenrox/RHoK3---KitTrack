02f928ed68046ef10980877b759d13864089614c
Ian Hopkins <i.hopkins@syncapse.com>
2011-06-05 09:34:47 -0400

    adding all the models to the admin

--- webui/admin.py
+++ webui/admin.py
@@ -1,6 +1,12 @@
-from webui.models import Location, KitType, Kit
+from webui.models import *
 from django.contrib import admin
 
 admin.site.register(Location)
 admin.site.register(KitType)
 admin.site.register(Kit)
+admin.site.register(KitState)
+admin.site.register(KitHistory)
+admin.site.register(CartonType)
+admin.site.register(SupplyItemType)
+admin.site.register(SupplyItem)
+admin.site.register(CartonContent)
